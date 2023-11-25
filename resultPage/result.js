b9f80bafb1dd0568142f349560a9253a21706a0b
Sahil Kharatmol <144338853+SahilK1720@users.noreply.github.com>
2023-11-25 10:38:41 +0530

    Add files via upload

---
+++ resultPage/result.js
@@ -0,0 +1,37 @@
+const winDetails = JSON.parse(localStorage.getItem("winDetails"));
+
+var moves = winDetails.moves;
+var handsWon = winDetails.handsWon;
+var handsPlayed = winDetails.handsPlayed;
+
+const text = document.getElementById("hands-won")
+const text2 = document.getElementById("numberMoves");
+
+text.innerHTML = `${handsWon} '/' ${handsPlayed}`;
+text2.innerHTML = moves; 
+
+
+const playAgain = document.getElementById("play-again");
+
+playAgain.addEventListener("click", function(){
+    location.href = "../index.html";
+})
+
+
+
+//Using arrays to store win phrases and using random number function to randomly display the phrase
+const win = document.getElementById("compliment");
+
+// Array of values
+const randomValues = ["Good Work!", "Hurray!", "Cheers!🥂", "Congratulations!", "Hear-Hear!"];
+
+// Function to get a random value from the array
+function getRandomValue(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+// Get a random value and display it
+const randomOutput = getRandomValue(randomValues);
+win.innerHTML = randomOutput;
+
